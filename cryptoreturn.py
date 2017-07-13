2b3d7b6a9db037c69b772fb4e981133f29e46f61
mistermaps <keaghoul@gmail.com>
2017-07-13 12:18:36 -0700

    aesthetic changes & error fixes

--- cryptoreturn.py
+++ cryptoreturn.py
@@ -14,27 +14,28 @@ import time
 import sys
 from coins import Coins
 
-
+done = False
 coinOpt = ['Bitcoin','Ether','Golem','Antshares']
 coinSel = []
 coinInvest = [False for i in range(len(coinOpt))]
 USER = os.path.expanduser('~')
+prompt = '>>\t'
 
 class spinner(threading.Thread):
-    
+
     def run(self):
             global stop
-            print ('Loading:')
+            print ('Calculating ROI:')
             #sys.stdout.flush()
             i = 0
             while stop != True:
-                    if (i%4) == 0: 
+                    if (i%4) == 0:
                     	sys.stdout.write('\b/')
-                    elif (i%4) == 1: 
+                    elif (i%4) == 1:
                     	sys.stdout.write('\b-')
-                    elif (i%4) == 2: 
+                    elif (i%4) == 2:
                     	sys.stdout.write('\b\\')
-                    elif (i%4) == 3: 
+                    elif (i%4) == 3:
                     	sys.stdout.write('\b|')
 
                     sys.stdout.flush()
@@ -53,17 +54,40 @@ def load(func):
 	s = spinner()
 	s.start()
 	try:
-		func()		
+		func()
 		stop = True
 	except KeyboardInterrupt or EOFError:
          stop = True
 
+def intro():
+	print("	               _                 _                    ")
+	print("  ___ _ __ _   _ _ __ | |_ ___  _ __ ___| |_ _   _ _ __ _ __  ")
+	print(" / __| '__| | | | '_ \| __/ _ \| '__/ _ \ __| | | | '__| '_ \ ")
+	print("| (__| |  | |_| | |_) | || (_) | | |  __/ |_| |_| | |  | | | |")
+	print(" \___|_|   \__, | .__/ \__\___/|_|  \___|\__|\__,_|_|  |_| |_|")
+	print("           |___/|_|  											")
+	print('\n Welcome to CryptoReturn, the cryptocurrency ROI calculator.')
+	print('\t\t\tWritten by MAPS.\n\n')
+	print("\tUse 'Enter' to enter in initial values.")
+	print("\tUse 'Update' to add or subtract investments.")
+	print("\tSelect 'Check' to calculate ROI.")
+	print('\nAfter any of these options, the program will calculate your ROI.\n')
+	#sel = input('Please select:'\n\t(E)nter\n\t(U)pdate\n\t(C)heck\n').lower()
+	print('Please select:')
+	time.sleep(.1)
+	print('\t(E)nter')
+	time.sleep(.1)
+	print('\t(U)pdate')
+	time.sleep(.1)
+	print('\t(C)heck')
+	sel = input(prompt)
+	return sel
 #function that reads from file an populates array with contents
 def parse():
 	global values
 	csv.register_dialect('crypto', delimiter=',')
 	#regular config is for Linux, uncomment below for windows; if the program breaks, update the path to reflect where the repo is
-	
+
 	#for windows (untested)
 	#with open(USER + '\cryptoreturn\\values.csv')
 
@@ -84,7 +108,7 @@ def enter():
 		clear()
 		#erases the file
 		with open(USER + '/cryptoreturn/values.csv','w'): pass
-		
+
 		f = open(USER + '/cryptoreturn/values.csv','r+')
 		print("This program currently supports: ")
 		#list coins currently built into program
@@ -94,6 +118,7 @@ def enter():
 		for i in range(len(coinOpt)):
 			#prompt user for coin investments
 			coinSel.insert(i, input("Do you have holdings in " + coinOpt[i] + "?\n").lower())
+			print(prompt)
 			if coinSel[i] == 'y' or coinSel[i] == 'yes':
 				coinInvest[i] = True
 		i = 0
@@ -101,7 +126,7 @@ def enter():
 			#bitcoin
 			if i == 0:
 				if coinInvest[i] == True:
-					btcAmount = float(input('Please enter the exact amount of BTC held:\n'))
+					btcAmount = float(input('Please enter the exact amount of BTC held:\n' + prompt))
 					if btcAmount > 0:
 						f.write('BTC:' + str(btcAmount) + ',')
 						btcTotal = float(input('Please enter the total amount spent on BTC:\n$'))
@@ -115,7 +140,7 @@ def enter():
 			#ethereum
 			if i == 1:
 				if coinInvest[i] == True:
-					ethAmount = float(input('Please enter the exact amount of ETH held:\n'))
+					ethAmount = float(input('Please enter the exact amount of ETH held:\n' + prompt))
 					if ethAmount > 0:
 						f.write('ETH:' + str(ethAmount) + ',')
 						ethTotal = float(input('Please enter the total amount spent on ETH:\n$'))
@@ -129,7 +154,7 @@ def enter():
 			#Golem
 			if i == 2:
 				if coinInvest[i] == True:
-					gntAmount = float(input('Please enter the exact amount of GNT held:\n'))
+					gntAmount = float(input('Please enter the exact amount of GNT held:\n' + prompt))
 					if gntAmount > 0:
 						f.write('GNT:' + str(gntAmount) + ',')
 						gntTotal = float(input('Please enter the total amount spent on GNT:\n$'))
@@ -137,13 +162,13 @@ def enter():
 					elif gntAmount == 0:
 						f.write('GNT:' + str(0) + ',')
 						f.write('GNT cost:' + str(0) + ',')
-				elif coinInvest[i] == False:	
+				elif coinInvest[i] == False:
 					f.write('GNT:' + str(0) + ',')
 					f.write('GNT cost:' + str(0) + ',')
 			#Antshares
 			if i == 3:
 				if coinInvest[i] == True:
-					ansAmount = float(input('Please enter the exact amount of ANS held:\n'))
+					ansAmount = float(input('Please enter the exact amount of ANS held:\n' + prompt))
 					if ansAmount > 0:
 						f.write('ANS:' + str(ansAmount) + ',')
 						ansTotal = float(input('Please enter the total amount spent on ANS:\n$'))
@@ -193,8 +218,9 @@ def update():
 	ansAmount = read.ansAmount
 	ansTotal = read.ansTotal
 	f = open(USER + '/cryptoreturn/values.csv','r+')
+	print('Please type (y)es or (n)o. Hitting enter or any other key defaults to no.')
 	for i in range(len(coinOpt)):
-			coinSel.insert(i, input("Update: " + coinOpt[i] + "? (Y)es\n").lower())
+			coinSel.insert(i, input("Update: " + coinOpt[i] + "?\n" + prompt).lower())
 			if coinSel[i] == 'y' or coinSel[i] == 'yes':
 				coinInvest[i] = True
 	i = 0
@@ -202,21 +228,21 @@ def update():
 		#bitcoin
 		if i == 0:
 			if coinInvest[i] == True:
-				sel = input('(B)uy or (s)ell BTC?\n').lower()
+				sel = input('(B)uy or (s)ell BTC?\n' + prompt).lower()
 				if sel == 'buy' or sel == 'b':
-					addBtcAmount = float(input('How much BTC are you adding?\n'))
+					addBtcAmount = float(input('How much BTC are you adding?\n' + prompt))
 					if addBtcAmount > 0:
 						btcAmount += addBtcAmount
 						f.write('BTC:' + str(btcAmount) + ',')
-						addBtcTotal = float(input('How much did you spend to add this amount?\n'))
+						addBtcTotal = float(input('How much did you spend to add this amount?\n$'))
 						btcTotal += addBtcTotal
 						f.write('BTC cost:' + str(btcTotal) + ',')
 				elif sel == 'sell' or 's':
-					addBtcAmount = float(input('How much BTC did you sell?\n'))
+					addBtcAmount = float(input('How much BTC did you sell?\n' + prompt))
 					if addBtcAmount > 0:
 						btcAmount -= addBtcAmount
 						f.write('BTC:' + str(btcAmount) + ',')
-						addBtcTotal = float(input('At what price did you sell?\n'))
+						addBtcTotal = float(input('At what price did you sell?\n$'))
 						btcTotal -= addBtcTotal
 						f.write('BTC cost:' + str(btcTotal) + ',')
 			elif coinInvest[i] == False:
@@ -225,21 +251,21 @@ def update():
 		#ethereum
 		if i == 1:
 			if coinInvest[i] == True:
-				sel = input('(B)uy or (s)ell ETH?\n').lower()
+				sel = input('(B)uy or (s)ell ETH?\n' + prompt).lower()
 				if sel == 'buy' or sel == 'b':
-					addEthAmount = float(input('How much ETH are you adding?\n'))
+					addEthAmount = float(input('How much ETH are you adding?\n' + prompt))
 					if addEthAmount > 0:
 						ethAmount += addEthAmount
 						f.write('ETH:' + str(ethAmount) + ',')
-						addEthTotal = float(input('How much did you spend to add this amount?\n'))
+						addEthTotal = float(input('How much did you spend to add this amount?\n$'))
 						ethTotal += addEthTotal
 						f.write('ETH cost:' + str(ethTotal) + ',')
 				elif sel == 'sell' or 's':
-					addEthAmount = float(input('How much ETH did you sell?\n'))
+					addEthAmount = float(input('How much ETH did you sell?\n' + prompt))
 					if addEthAmount > 0:
 						ethAmount -= addEthAmount
 						f.write('ETH:' + str(ethAmount) + ',')
-						addEthTotal = float(input('At what price did you sell?\n'))
+						addEthTotal = float(input('At what price did you sell?\n$'))
 						ethTotal -= addEthTotal
 						f.write('ETH cost:' + str(ethTotal) + ',')
 			elif coinInvest[i] == False:
@@ -248,50 +274,50 @@ def update():
 			#Golem
 		if i == 2:
 			if coinInvest[i] == True:
-				sel = input('(B)uy or (s)ell GNT?\n').lower()
+				sel = input('(B)uy or (s)ell GNT?\n' + prompt).lower()
 				if sel == 'buy' or sel == 'b':
-					addGNTAmount = float(input('How much GNT are you adding?\n'))
+					addGNTAmount = float(input('How much GNT are you adding?\n' + prompt))
 					if addGNTAmount > 0:
 						gntAmount += addGNTAmount
 						f.write('GNT:' + str(gntAmount) + ',')
-						addGNTTotal = float(input('How much did you spend to add this amount?\n'))
+						addGNTTotal = float(input('How much did you spend to add this amount?\n$'))
 						gntTotal += addGNTTotal
 						f.write('GNT Cost:' + str(gntTotal) + ',')
 				elif sel == 'sell' or 's':
-					addGNTAmount = float(input('How much GNT did you sell?\n'))
+					addGNTAmount = float(input('How much GNT did you sell?\n' + prompt))
 					if addGNTAmount > 0:
 						gntAmount -= addGNTAmount
 						f.write('GNT:' + str(gntAmount) + ',')
-						addGNTTotal = float(input('At what price did you sell?\n'))
+						addGNTTotal = float(input('At what price did you sell?\n$'))
 						gntTotal -= addGNTTotal
 						f.write('GNT cost:' + str(gntTotal) + ',')
-			elif coinInvest[i] == False:	
+			elif coinInvest[i] == False:
 				f.write('GNT:' + str(gntAmount) + ',')
 				f.write('GNT cost:' + str(gntTotal) + ',')
 			#Antshares
 		if i == 3:
 			if coinInvest[i] == True:
-				sel = input('(B)uy or (s)ell ANS?\n').lower()
+				sel = input('(B)uy or (s)ell ANS?\n' + prompt).lower()
 				if sel == 'buy' or sel == 'b':
-					addANSAmount = float(input('How much ANS are you adding?\n'))
+					addANSAmount = float(input('How much ANS are you adding?\n' + prompt))
 					if addANSAmount > 0:
 						ansAmount += addANSAmount
 						f.write('ANS:' + str(ansAmount) + ',')
-						addANSTotal = float(input('How much did you spend to add this amount?\n'))
+						addANSTotal = float(input('How much did you spend to add this amount?\n$'))
 						ansTotal += addANSTotal
 						f.write('ANS cost:' + str(ansTotal) + ',')
 				elif sel == 'sell' or 's':
-					addANSAmount = float(input('How much ANS did you sell?\n'))
+					addANSAmount = float(input('How much ANS did you sell?\n' + prompt))
 					if addANSAmount > 0:
 						ansAmount -= addANSAmount
 						f.write('ANS:' + str(ansAmount) + ',')
-						addANSTotal = float(input('At what price did you sell?\n'))
+						addANSTotal = float(input('At what price did you sell?\n$'))
 						ansTotal -= addANSTotal
 						f.write('ANS cost:' + str(ansTotal) + ',')
 				elif coinInvest[i] == False:
 						f.write('ANS:' + str(ansAmount) + ',')
 						f.write('ANS cost:' + str(ansTotal) + ',')
-		i = i + 1	
+		i = i + 1
 	clear()
 #function to get current price from API
 def getPrices():
@@ -344,6 +370,8 @@ def roi():
 			coinInvest[l] = True
 		l = l + 1
 	i = 0
+	clear()
+	done = True
 	while i < len(coinOpt):
 		if i == 0 and coinInvest[i] == True:
 			print('Current Bitcoin Price: \n\t\t\t$%.2f' % btcPrice)
@@ -364,7 +392,7 @@ def roi():
 			print('Current ETH ROI: \n\t\t\t%s' % str(change) + '$%.2f' % abs(ethTotal - ethValue))
 			print()
 		elif i == 1 and coinInvest[i] == False:
-			print("\nNo " + coinOpt[i] + " holdings to display.\n")		
+			print("\nNo " + coinOpt[i] + " holdings to display.\n")
 		if i == 2 and coinInvest[i] == True:
 			print('Current Golem Price: \n\t\t\t$%.2f' % gntPrice)
 			print('Current GNT holdings: \n\t\t\t%.7f' % gntAmount)
@@ -393,27 +421,15 @@ def roi():
 
 #begin user interactions
 def main():
-	print("	               _                 _                    ")
-	print("  ___ _ __ _   _ _ __ | |_ ___  _ __ ___| |_ _   _ _ __ _ __  ")
-	print(" / __| '__| | | | '_ \| __/ _ \| '__/ _ \ __| | | | '__| '_ \ ")
-	print("| (__| |  | |_| | |_) | || (_) | | |  __/ |_| |_| | |  | | | |")
-	print(" \___|_|   \__, | .__/ \__\___/|_|  \___|\__|\__,_|_|  |_| |_|")
-	print("           |___/|_|  											")
-	print('\n Welcome to CryptoReturn, the cryptocurrency ROI calculator.')
-	print('\t\t\tWritten by MAPS.\n\n')
-	print("\tUse 'Enter' to enter in initial values.")
-	print("\tUse 'Update' to add or subtract investments.")
-	print("\tSelect 'Check' to calculate ROI.")
-	print('\nAfter any of these options, the program will calculate your ROI.\n')
-	sel = input('Please select:\n\t(E)nter\n\t(U)pdate\n\t(C)heck\n').lower()
+	clear()
+	sel = intro()
 	if sel == 'check' or sel == 'c':
 		clear()
 	elif sel == 'update' or sel == 'u':
 		update()
 	elif sel == 'enter' or sel == 'e':
 		enter()
-	load(roi)
 	#roi()
-		
+	load(roi)
 main()
 stop = True
