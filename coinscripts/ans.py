f57f55369c064fbcbbc3d3dac34992cbaf8722b9
mistermaps <keaghoul@gmail.com>
2017-07-26 17:46:53 -0700

    update after namechange

--- coinscripts/ans.py
+++ coinscripts/ans.py
@@ -2,7 +2,7 @@
 import urllib.request
 import json
 def ans():
-	with urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/antshares/") as url:
+	with urllib.request.urlopen("https://api.coinmarketcap.com/v1/ticker/neo/") as url:
 		data = json.loads(url.read().decode())
 		data = str(data).split()
 		ansPrice = float(str(data[9]).replace("'","").replace(',',''))
